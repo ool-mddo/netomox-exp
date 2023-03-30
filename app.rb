7c586ec779554038105ad17e6beb3f892e4ef7e1
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2023-03-30 15:47:23 +0900

    Update snapshot layer nodes/interfaces api

--- app.rb
+++ app.rb
@@ -227,12 +227,12 @@ module NetomoxExp
                 network, snapshot, layer = %i[network snapshot layer].map { |key| params[key] }
                 topology_data = read_topology_file(network, snapshot)
                 ns_converter = ns_converter_wo_topology(network)
-                converter = BatfishConverter.new(topology_data, layer, ns_converter)
+                bf_converter = BatfishConverter.new(topology_data, layer, ns_converter)
                 # response
-                converter.convert
+                bf_converter.convert
               end
 
-              desc 'convert layer data to containerl-lab topology json'
+              desc 'convert layer data to container-lab topology json'
               params do
                 optional :env_name, type: String, desc: 'Environment name (for container-lab)', default: 'emulated'
               end
@@ -240,15 +240,14 @@ module NetomoxExp
                 network, snapshot, layer, env_name = %i[network snapshot layer env_name].map { |key| params[key] }
                 topology_data = read_topology_file(network, snapshot)
                 ns_converter = ns_converter_wo_topology(network)
-                converter = ContainerLabConverter.new(topology_data, layer, ns_converter, { env_name: })
+                clab_converter = ContainerLabConverter.new(topology_data, layer, ns_converter, { env_name: })
                 # response
-                converter.convert
+                clab_converter.convert
               end
 
-              desc 'Get nodes in the layer'
+              desc 'Get all nodes in the layer'
               params do
                 optional :node_type, type: String, desc: 'Node type (segment/node/endpoint)'
-                optional :name_type, type: String, desc: 'Converted name type (l3/l1_agent/l1_principal)', default: 'l3'
               end
               get 'nodes' do
                 network, snapshot, layer = %i[network snapshot layer].map { |key| params[key] }
@@ -256,15 +255,49 @@ module NetomoxExp
                 nw = nws.find_network(layer)
                 error!("#{network}/#{snapshot}/#{layer} not found", 404) if nw.nil?
 
-                nodes = if params.key?(:node_type)
-                          nw.nodes.select { |node| node.attribute.node_type == params[:node_type] }.map(&:name)
-                        else
-                          nw.nodes.map(&:name)
-                        end
+                ns_converter = ns_converter_wo_topology(network)
+                nodes = nw.nodes.map
+                nodes = nw.nodes.select { |n| n.attribute.node_type == params[:node_type] } if params.key?(:node_type)
+
+                # response
+                nodes.map do |node|
+                  {
+                    node: node.name,
+                    alias: ns_converter.node_name_table.find_l1_alias(node.name),
+                    attribute: node.attribute.to_data
+                  }
+                end
+              end
+
+              desc 'Get all interfaces in the layer'
+              params do
+                optional :node_type, type: String, desc: 'Node type (segment/node/endpoint)'
+              end
+              get 'interfaces' do
+                network, snapshot, layer = %i[network snapshot layer].map { |key| params[key] }
+                nws = read_topology_instance(network, snapshot)
+                nw = nws.find_network(layer)
+                error!("#{network}/#{snapshot}/#{layer} not found", 404) if nw.nil?
 
                 ns_converter = ns_converter_wo_topology(network)
+                nodes = nw.nodes
+                nodes = nw.nodes.select { |n| n.attribute.node_type == params[:node_type] } if params.key?(:node_type)
+
                 # response
-                nodes.map { |node| ns_converter.node_name_table.find_l1_alias(node)[params[:name_type]] }
+                nodes.map do |node|
+                  {
+                    node: node.name,
+                    alias: ns_converter.node_name_table.find_l1_alias(node.name),
+                    attribute: node.attribute.to_data,
+                    interfaces: node.termination_points.map do |tp|
+                      {
+                        interface: tp.name,
+                        alias: ns_converter.tp_name_table.find_l1_alias(node.name, tp.name),
+                        attribute: tp.attribute.to_data
+                      }
+                    end
+                  }
+                end
               end
             end
           end
