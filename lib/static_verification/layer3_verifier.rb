028819aae5df2aea7a98ef26490a179a1402b123
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2023-08-24 10:33:37 +0900

    Add L3 overlap ip address check

--- lib/static_verification/layer3_verifier.rb
+++ lib/static_verification/layer3_verifier.rb
@@ -22,6 +22,7 @@ module NetomoxExp
 
           # for each segment-node
           verify_segment_prefix_ip(node)
+          verify_segment_ip_overlap(node)
         end
         verify_segment_prefix_overlap
 
@@ -30,14 +31,20 @@ module NetomoxExp
 
       private
 
-      # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity
+      # @param [Netomox::Topology::TermPoint] term_point Term-point (L3)
+      # @return [Array<IPAddress::IPv4>] IP addresses of the term-point
+      def all_ip_addrs_by_tp(term_point)
+        term_point.attribute.ip_addrs.map { |ip| IPAddress::IPv4.new(ip) }
+      end
+
+      # rubocop:disable Metrics/AbcSize
 
       # @param [Array<IPAddress::IPv4>] seg_prefixes Prefix-list of a segment node
       # @param [Netomox::Topology::Link] seg_src_link A link that source is a segment node
       # @return [void]
       def verify_seg_prefix_and_tp_ip(seg_prefixes, seg_src_link)
         _, node_tp = find_node_tp_by_edge(seg_src_link.destination)
-        tp_ip_addrs = node_tp.attribute.ip_addrs.map { |ip| IPAddress::IPv4.new(ip) }
+        tp_ip_addrs = all_ip_addrs_by_tp(node_tp)
 
         add_log_message(:error, node_tp.path, 'Term-point has multiple ip-addresses') if tp_ip_addrs.length > 1
         add_log_message(:error, node_tp.path, 'Term-point does not have ip-address') if tp_ip_addrs.empty?
@@ -49,7 +56,7 @@ module NetomoxExp
           add_log_message(:error, node_tp.path, msg)
         end
       end
-      # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity
+      # rubocop:enable Metrics/AbcSize
 
       # @param [Netomox::Topology::Node] seg_node Segment node
       # @param [Array<IPAddress::IPv4>] seg_prefixes Prefix-list of the segment node
@@ -93,12 +100,11 @@ module NetomoxExp
       # @return [Array<IPAddress::IPv4>] Overlapped prefixes
       def find_all_overlap_prefixes
         all_prefixes = all_segment_prefixes
-        puts "# DEBUG: all_prefixes : #{all_prefixes.map(&:to_string)}"
-        overlap_prefixes = all_prefixes.map do |prefix|
+        overlap_prefixes = all_prefixes.map do |target_prefix|
           # ignore itself (#equal? compares object_id) and find all overlapped prefixes
-          all_prefixes.reject { |p| p.equal?(prefix) }.find_all { |p| prefix.include?(p) }
+          all_prefixes.reject { |prefix| prefix.equal?(target_prefix) }
+                      .find_all { |prefix| target_prefix.include?(prefix) }
         end
-        puts "# DEBUG: overlap_prefixes : #{overlap_prefixes.flatten.map(&:to_string)}"
         overlap_prefixes.flatten
       end
 
@@ -116,6 +122,34 @@ module NetomoxExp
         find_all_overlap_prefixes.each do |prefix|
           find_all_seg_node_by_prefix(prefix).each do |node|
             add_log_message(:warn, node.path, "It has overlapped prefix: #{prefix}")
+          end
+        end
+      end
+
+      # @param [Netomox::Topology::Node] seg_node Segment node
+      # @return [Array<Netomox::Topology::TermPoint>] Termination-point connected with the segment
+      def all_node_tps_connected_segment(seg_node)
+        seg_node.termination_points.map do |seg_tp|
+          seg_src_link = @target_nw.find_link_by_source(seg_node.name, seg_tp.name)
+          _, node_tp = find_node_tp_by_edge(seg_src_link.destination)
+          node_tp
+        end
+      end
+
+      # @param [Netomox::Topology::Node] seg_node Segment node
+      # @return [void]
+      def verify_segment_ip_overlap(seg_node)
+        all_tps = all_node_tps_connected_segment(seg_node)
+
+        all_tps.each do |target_tp|
+          all_ip_addrs_by_tp(target_tp).each do |target_ip|
+            # ignore itself (#equal? compares object_id) and find all tp which owned overlapped ip
+            overlap_tps = all_tps.reject { |tp| tp.equal?(target_tp) }.find_all do |tp|
+              all_ip_addrs_by_tp(tp).any? { |ip| target_ip == ip }
+            end
+            next if overlap_tps.empty?
+
+            add_log_message(:error, target_tp.path, "It has overlapped ip address: #{target_ip}")
           end
         end
       end
