7f0fe92f11c389d2849756a17d457cd77ae89106
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2024-01-03 15:08:56 +0900

    Add bgp-policy reference check

--- lib/static_verification/bgp_proc_verifier.rb
+++ lib/static_verification/bgp_proc_verifier.rb
@@ -5,6 +5,8 @@ require_relative 'verifier_base'
 
 module NetomoxExp
   module StaticVerifier
+    # rubocop:disable Metrics/ClassLength
+
     # bgp-proc verifier
     class BgpProcVerifier < VerifierBase
       # @param [Netomox::Topology::Networks] networks Networks object
@@ -18,10 +20,93 @@ module NetomoxExp
         verify_layer(severity) do
           verify_all_links { |bgp_proc_link| verify_peer_params(bgp_proc_link) }
           verify_all_node_tps { |bgp_proc_node, bgp_proc_tp| verify_node_tp_asn(bgp_proc_node, bgp_proc_tp) }
+          verify_all_nodes { |bgp_proc_node| verify_bgp_policy_refs(bgp_proc_node) }
         end
       end
 
       private
+
+      # @param [Netomox::Topology::MddoBgpProcNodeAttribute] attribute Node attribute (bgp-proc)
+      # @param [Netomox::Topology::MddoBgpPolicyAction] action Action of a bgp-policy statement
+      # @return [nil, Netomox::Topology::SubAttributeBase] nil if not found reference
+      def find_bgp_policy_action_ref(attribute, action)
+        case action
+        when Netomox::Topology::MddoBgpPolicyActionApply
+          attribute.policies.find { |p| p.name == action.apply }
+        when Netomox::Topology::MddoBgpPolicyActionCommunity
+          attribute.community_sets.find { |c| c.name == action.community.name }
+        else
+          action
+        end
+      end
+
+      # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
+
+      # @param [Netomox::Topology::MddoBgpProcNodeAttribute] attribute Node attribute (bgp-proc)
+      # @param [Netomox::Topology::MddoBgpPolicyCondition] condition Condition of a bgp-policy statement
+      # @return [nil, Array<Hash>, Netomox::Topology::SubAttributeBase] nil if not found reference
+      #   NOTE: Array<Hash>...check result of multiple conditions
+      def find_bgp_policy_condition_ref(attribute, condition)
+        case condition
+        when Netomox::Topology::MddoBgpPolicyConditionPolicy
+          attribute.policies.find { |p| p.name == condition.policy }
+        when Netomox::Topology::MddoBgpPolicyConditionAsPathGroup
+          attribute.as_path_sets.find { |a| a.group_name == condition.as_path_group }
+        when Netomox::Topology::MddoBgpPolicyConditionCommunity
+          condition.communities.map do |cond_community|
+            result = attribute.community_sets.find { |c| c.name == cond_community }
+            { result:, condition: }
+          end
+        when Netomox::Topology::MddoBgpPolicyConditionPrefixList
+          attribute.prefix_sets.find { |p| p.name == condition.prefix_list }
+        when Netomox::Topology::MddoBgpPolicyConditionPrefixListFilter
+          attribute.prefix_sets.find { |p| p.name == condition.prefix_list_filter.prefix_list }
+        else
+          condition
+        end
+      end
+      # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
+
+      # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity, Metrics/BlockLength
+
+      # @param [Netomox::Topology::Node] node Node (bgp-proc)
+      # @return [void]
+      def verify_bgp_policy_refs(node)
+        node.attribute.policies.each do |policy|
+          policy.default.actions.each do |action|
+            next if find_bgp_policy_action_ref(node.attribute, action)
+
+            msg = "Action reference is not found, #{action.to_data} in statement:default of policy:#{policy.name}"
+            add_log_message(:error, node.path, msg)
+          end
+          policy.statements.each do |statement|
+            msg_suffix = "in statement:#{statement.name} of policy:#{policy.name}"
+            statement.actions.each do |action|
+              next if find_bgp_policy_action_ref(node.attribute, action)
+
+              msg = "Action reference is not found, #{action.to_data} #{msg_suffix}"
+              add_log_message(:error, node.path, msg)
+            end
+            statement.conditions.each do |condition|
+              find_data = find_bgp_policy_condition_ref(node.attribute, condition)
+              if find_data.is_a?(Array)
+                find_data.each do |find_datum|
+                  next if find_datum[:result]
+
+                  msg = "Condition reference is not found, #{condition.to_data};#{find_datum[:condition]} #{msg_suffix}"
+                  add_log_message(:error, node.path, msg)
+                end
+                next
+              end
+              next if find_data
+
+              msg = "Condition reference is not found, #{condition.to_data} #{msg_suffix}"
+              add_log_message(:error, node.path, msg)
+            end
+          end
+        end
+      end
+      # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity, Metrics/BlockLength
 
       # @param [Netomox::Topology::Node] node Node (bgp-proc)
       # @param [Netomox::Topology::TermPoint] term_point TermPoint of the node (bgp-proc)
@@ -81,5 +166,6 @@ module NetomoxExp
         verify_timer(link.path, src_tp, dst_tp)
       end
     end
+    # rubocop:enable Metrics/ClassLength
   end
 end
